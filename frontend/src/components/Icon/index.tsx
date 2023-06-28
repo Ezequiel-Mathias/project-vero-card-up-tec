32b716224d69f9545f2494029ef94a7b822b1350
ezequiel <ezequiel@uptechnology.local>
2023-06-28 16:30:15 -0300

    Making the Icons component

---
+++ frontend/src/components/Icon/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface IIconProps extends React.HTMLProps<HTMLSpanElement> {
+    name : string
+}
+
+
+const Icon: React.FC<IIconProps> = (props) => {
+    return (
+        <span {...props} className="icon material-symbols-rounded">
+            {props.name}
+        </span>
+    )
+}
+
+export default Icon;
