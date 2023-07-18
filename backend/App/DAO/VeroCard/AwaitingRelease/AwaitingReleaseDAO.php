935bf7f8456ecc139cf0f79b8c9c3b3a5a6ce175
ezequiel <ezequiel@uptechnology.local>
2023-07-18 14:30:47 -0300

    Initialization of data brought from the database

--- backend/App/DAO/VeroCard/AwaitingRelease/AwaitingReleaseDAO.php
+++ backend/App/DAO/VeroCard/AwaitingRelease/AwaitingReleaseDAO.php
@@ -14,7 +14,7 @@ class AwaitingReleaseDAO extends Connection{
     public function getAllAwaitingRelease() : array {
 
         $productsAwaitingRelease = $this -> pdo
-            ->query("") 
+            ->query("SELECT * FROM ordem_producao_status WHERE id_cliente = 34 AND status = 0;") 
             ->fetchAll(\PDO::FETCH_ASSOC);
 
             return $productsAwaitingRelease;
