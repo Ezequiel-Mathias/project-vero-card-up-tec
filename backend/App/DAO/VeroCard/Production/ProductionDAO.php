e5ac03547bff5f2b161f39fd5131552c69916f50
ezequiel <ezequiel@uptechnology.local>
2023-07-18 12:44:12 -0300

    Routes awaiting release and in production

--- backend/App/DAO/VeroCard/Production/ProductionDAO.php
+++ backend/App/DAO/VeroCard/Production/ProductionDAO.php
@@ -12,26 +12,7 @@ class ProductionDAO extends Connection{
 
     public function getAllProductsInProduction() : array {
         $products = $this -> pdo
-            ->query("SELECT a.id_op,
-            c.nome_arquivo,
-            a.qtd_objs,
-            CASE
-              WHEN a.idlayoutarquivo = 50 THEN 'CHIP'::text
-              WHEN a.idlayoutarquivo = 76 THEN 'TARJA'::text
-              ELSE NULL::text
-            END AS tipo,
-            a.dt_op,
-            a.nr_lote,
-            (
-              SELECT ordem_producao_status.dt_finalizado
-              FROM ordem_producao_status
-              WHERE ordem_producao_status.id_op = a.id_op AND
-                    ordem_producao_status.status = 4
-            ) AS dt_expedicao
-     FROM ordem_producao a
-          JOIN ordem_producao_status b ON a.id_op = b.id_op
-          JOIN ordem_producao_detalhe c ON a.id_op = c.id_op
-     WHERE a.id_cliente = 34 GROUP BY a.id_op , c.nome_arquivo,  a.qtd_objs;")  
+            ->query("")  
             ->fetchAll(\PDO::FETCH_ASSOC);
 
             return $products;
